c29540c9c669ea3509262e0c4a662494fca5e635
kaustubh babar <kaustubh.babar@intelliconnect-tech.com>
2021-02-22 13:08:44 +0530

    Added Cors support(for development only)

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ mod vars;
 
 use actix_web::{web, App, HttpServer};
 use dotenv::dotenv;
+use actix_cors::Cors;
+
 use controller::{home};
 use middleware::auth;
 
@@ -26,12 +28,17 @@ async fn main() -> std::io::Result<()> {
     info!("starting server");
     //Configure and Start New HTTP server
     HttpServer::new(move || {
+        //Cors::permissive() should only be used development purposes, not to be used in production
+        let cors = Cors::permissive();
+
         App::new()
+            .wrap(cors)
             .wrap(auth::Auth)
             .wrap(actix_web::middleware::Logger::default())
             .route("/login", web::post().to(home::login))
             .route("/view_holidays", web::get().to(home::fetch_holidays))
             .route("/register_user", web::post().to(home::register_user))
+            
             
     })
     .bind(format!("{}:{}", vars::domain(), vars::port()))?
